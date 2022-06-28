47882333fb4882cd112616e16b6630d60ad0f589
jKm00 <joakied@stud.ntnu.no>
2022-06-28 19:23:30 +0200

    Added styling

--- svelte-review-app/src/App.svelte
+++ svelte-review-app/src/App.svelte
@@ -59,7 +59,7 @@
 	]
 
 	let reviewId = 0
-	let reviews: Review[] = []
+	let reviews: Review[] = [{ id: reviewId++, rating: 10, feedback: 'Just a test' }]
 	let selected: number
 	let feedback: string
 
@@ -79,22 +79,26 @@
 </script>
 
 <main>
-	<form on:submit|preventDefault={handleSubmit}>
+	<form on:submit|preventDefault={handleSubmit} class="box">
 		<fieldset>
 			<legend>How would you rate your service with us?</legend>
-			{#each options as option}
-				<label for={option.id}>{option.label}</label>
-				<input id={option.id} bind:group={selected} type="radio" value={option.value} name="rating" />
-			{/each}
-			<input bind:value={feedback} type="text">
-			<button type="submit" disabled={feedback ? feedback.length < 10 : true}>Send</button>
+			<div class="radio-wrapper">
+				{#each options as option}
+					<input id={option.id} bind:group={selected} type="radio" value={option.value} name="rating" />
+					<label for={option.id}>{option.label}</label>
+				{/each}
+			</div>
+			<div class="feedback-wrapper">
+				<input bind:value={feedback} type="text" placeholder="Tell us something that keeps you comming back">
+				<button class="submit" type="submit" disabled={feedback ? feedback.length < 10 : true}>Send</button>
+			</div>
 		</fieldset>
 	</form>
-	<div>
+	<div class="stats">
 		<p>{reviewCount} Reviews</p>
 		<p>{average ? 'Ratings Average: ' + average : 'No ratings'}</p>
 	</div>
-	<div>
+	<div class="review-wrapper">
 		{#each reviews as review (review.id)}
 			<ReviewView review={review} on:remove={removeReview} />
 		{/each}
@@ -102,4 +106,88 @@
 </main>
 
 <style>
+	main {
+		max-width: 50rem;
+		margin: auto;
+		padding: 2rem 0;
+	}
+
+	form {
+		background-color: #fff;
+		padding: 2rem;
+	}
+
+	legend {
+		text-align: center;
+		font-weight: bold;
+		font-size: 1.5rem;
+		max-width: 25rem;
+		margin-bottom: 1rem;
+	}
+
+	.radio-wrapper {
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 2rem;
+	}
+
+	label {
+		--size: 3rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #eee;
+		height: var(--size);
+		width: var(--size);
+		border-radius: 100vh;
+	}
+
+	label:hover {
+		background-color: #FFB6C1;
+	}
+
+	input[type="radio"]:checked + label {
+		background-color: #FF69B4;
+		color: #fff;
+	}
+
+	input[type="radio"] {
+		display: none;
+	}
+
+	.feedback-wrapper {
+		position: relative;
+	}
+
+	.feedback-wrapper input {
+		width: 100%;
+		padding: 0.75rem;
+		border-radius: 0.5rem;
+		border: 1px solid #aaa;
+	}
+
+	.feedback-wrapper button {
+		position: absolute;
+		right: 0.5rem;
+		top:50%;
+		transform: translateY(-50%);
+		background-color: #111;
+		color: #fff;
+		padding: 0.5rem 1.5rem;
+		border-radius: 0.5rem;
+	}
+
+	.stats {
+		display: flex;
+		justify-content: space-between;
+		color: #fff;
+		font-weight: bold;
+		margin: 1rem 0;
+	}
+
+	.review-wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: 1.5rem;
+	}
 </style>
--- svelte-review-app/src/components/ReviewView.svelte
+++ svelte-review-app/src/components/ReviewView.svelte
@@ -10,12 +10,38 @@
   }
 </script>
 
-<div>
+<div class="box review">
   <span>{review.rating}</span>
   <p>{review.feedback}</p>
   <button on:click={handleClick}>x</button>
 </div>
 
 <style>
+  .review {
+    position: relative;
+    padding: 1rem 2rem;
+  }
 
+  .review span {
+    --size: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: calc(var(--size) / 2.5 * -1);
+    left: calc(var(--size) / 2.5 * -1);
+    background-color: #FF69B4;
+    color: #fff;
+    height: var(--size);
+    width: var(--size);
+    border-radius: 100vh;
+  }
+
+  .review button {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    font-weight: bold;
+    font-size: 1rem;
+  }
 </style>
